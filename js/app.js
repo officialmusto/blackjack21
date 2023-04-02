c77c94758bbbcccbe5dc3e5d24ad612090210d3d
musto <christianmustoj@gmail.com>
2023-04-01 23:17:56 -0400

    CAN NOW STORE CARDS IN DECKS CLASS. NOW WORKING ON CATCHING DUPLICATES>

--- js/app.js
+++ js/app.js
@@ -7,22 +7,29 @@ const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'
 
 // Establishing Deck Class to handle disposed deck, dealer deck, player deck, and the sorted deck.
 class Deck {
-  constructor(cards){
-    this.cards = cards
+  constructor(){
+    this.disposed = []
+    this.p1 = []
+    this.dealer = []
+    this.pullFrom = []
   }
 }
 
 // Establishing Cards Class to create an object with two key/value pairs that will be the selected random card from the 'HIT button.
-  class Card{
-    constructor(suit, value){
-      this.suit = suit
-      this.value = value
+class Card{
+  constructor(suit, value){
+    this.suit = suit
+    this.value = value
   }
 }
 
-let chosenCards = []
+
+let chosenCard = new Card()
+
+// Created a variable to utilize the '.push()' method and push the selected card into the dispose pile.
+let decks = new Deck()
 /* ------------------- CACHED REFERENCES ------------------- */
-pickCardBtn = document.getElementById('pick-card-btn')
+const pickCardBtn = document.getElementById('pick-card-btn')
 
 
 
@@ -33,19 +40,52 @@ pickCardBtn.addEventListener('click', handleClick)
 
 
 /* ------------------- FUNCTIONS ------------------- */
-function handleClick(){
+
+
+init()
+
+function init(){}
+
+function handleClick() {
+  checkForDuplicates()
+  generateCard()
+  render()
+}
+  
+  function generateCard(){
   // Picks random Suit from suits array.
-let suitIdx = Math.floor(Math.random() * suits.length)
-let suitPicked = suits[suitIdx]
+  let suitIdx = Math.floor(Math.random() * suits.length)
+  let suitPicked = suits[suitIdx]
 
-// Picks random Value form values array.
-let valueIdx = Math.floor(Math.random() * values.length)
-let valuePicked = values[valueIdx]
-cardPicked = (`${valuePicked} ${suitPicked}`)
-chosenCards.push(cardPicked)
-render()
-}
+  // Picks random Value from values array.
+  let valueIdx = Math.floor(Math.random() * values.length)
+  let valuePicked = values[valueIdx]
 
-function render(){
-console.log(chosenCards)
+  // Combines both value & suit into an object within Card class.
+  let chosenCard = new Card(suitPicked, valuePicked)
+
+  // Sends chosen card to disposed pile. //! WILL BE CHANGED TO A MORE-APPROPIATE DECK.
+  decks.disposed.push(chosenCard)
 }
+  
+  // Checks for Duplicates in decks Class. 
+  function checkForDuplicates() {
+    let isDuplicate = false;
+  
+    //? FIGURE OUT HOW TO NOT STORE DUPLICATES.
+    decks.disposed.forEach(function(disposedCard) {
+      disposedCard = console.log(disposedCard)
+      // if (disposedCard.suit === chosenCard.suit && disposedCard.value === chosenCard.value) {
+      //   isDuplicate = true
+      // }
+    })
+  
+    return isDuplicate;
+  }
+  
+
+//? HOW TO MAKE THIS FUNCTION NEEDED? RE-WATCH OLD LECTURE
+  // Renders Turn of P1
+  function render(){
+    console.log(decks.disposed)
+  }
