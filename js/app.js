e666e6a1760f044d9a0ea514d2d09a5d60c14d5b
musto <christianmustoj@gmail.com>
2023-04-03 12:53:59 -0400

    Adding new SVG files and utilizing a bit of CSS to create a visual

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 const suits = ['♠','♥', '♣', '♦']
 const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
+let playerOneHand = []
 
 // Establishing Deck Class to handle disposed deck, dealer deck, player deck, and the sorted deck.
 class Deck {
@@ -30,57 +31,50 @@ class Card{
 let decks = new Deck()
 let disposedDeck = decks.disposed
 /* ------------------- CACHED REFERENCES ------------------- */
-const pickCardBtn = document.getElementById('pick-card-btn')
+const hitBtn = document.getElementById('hit-btn')
 
 
 
 
 /* ------------------- EVENT LISTENERS ------------------- */
-pickCardBtn.addEventListener('click', handleClick)
+hitBtn.addEventListener('click', handleClick)
 
 
 
 /* ------------------- FUNCTIONS ------------------- */
 
-
 init()
 
-function init(){}
+function init(){
+  
+}
 
 function handleClick() {
-  // checkForDuplicates()
   generateCard()
   render()
 }
 
 function generateCard(){
-// Checks to see if 52 cards are in Deck.disposed Class array
   if (disposedDeck.length === 52){
+    hitBtn.disabled = true
     return
   }
-  // Picks random Suit from suits array.
-  let suitIdx = Math.floor(Math.random() * suits.length)
-  let suitPicked = suits[suitIdx]
-  
-  // Picks random Value from values array.
-  let valueIdx = Math.floor(Math.random() * values.length)
-  let valuePicked = values[valueIdx]
-  
-  // Combines both value & suit into an object within Card class.
-  let chosenCard = new Card(suitPicked, valuePicked)
-  
-// SOLVED DUPLICATES ISSUE - HUNTER LONG
-//IF DUPLICATE === TRUE, GENERATE NEW CARD, ELSE PUSH CHOSEN CARD TO ARRAY
+  suitIdx = Math.floor(Math.random() * suits.length)
+  suitPicked = suits[suitIdx]
+  valueIdx = Math.floor(Math.random() * values.length)
+  valuePicked = values[valueIdx]
+  chosenCard = new Card(suitPicked, valuePicked)
   if (checkDecks(chosenCard)){
   generateCard()
 }   else {
-  // Sends chosen card to disposed pile.
   disposedDeck.push(chosenCard)
   chosenCard = new Card()
 }
 }
 
+function playerOneTurn(){
 
+}
 
 function checkDecks(card){
   let duplicate = false
@@ -106,7 +100,7 @@ function checkDecks(card){
 
 
 
-  
+
   //todo CREATE BET MECHANICS
   //todo CREATE PLAYER 1 FIRST TURN LOGIC
   //todo CREATE DEALER TURN LOGIC
